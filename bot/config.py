d235bc1e0e4b39c1ea2236f93c7531094345c11a
Reinier Romero <85958980+reinierromero147@users.noreply.github.com>
2023-12-23 18:31:04 -0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,23 +3,23 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("6660807180:AAFtVdxLF2XngwzYJLY_xgTcws3zrlupB_Y")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
     SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
 
-    API_ID = int(os.environ.get("20946968"))
+    API_ID = int(os.environ.get("API_ID"))
 
-    API_HASH = os.environ.get("2e51eed71c861b8f030f959bc34c5167")
+    API_HASH = os.environ.get("API_HASH")
 
-    CLIENT_ID = os.environ.get("473810559979-b0nuk0tbb57at6ia16ltg8ukdiltdfdp.apps.googleusercontent.com")
+    CLIENT_ID = os.environ.get("CLIENT_ID")
 
-    CLIENT_SECRET = os.environ.get("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")
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
-    BOT_OWNER = int(os.environ.get("1437976419"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 
     AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
 
-    AUTH_USERS = [BOT_OWNER, 1437976419] + (
+    AUTH_USERS = [BOT_OWNER, 374321319] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
         if AUTH_USERS_TEXT
         else []
